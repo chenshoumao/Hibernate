935aba1f3b02ef67342c0f4bf9db4053b2cb0000
chenshoumao <1223991507@qq.com>
2017-08-24 18:17:38 +0800

    121

--- src/main/java/com/csm/controller/Test.java
+++ src/main/java/com/csm/controller/Test.java
@@ -5,6 +5,7 @@ import java.sql.DriverManager;
 
 public class Test {
 	public static void main(String[] args) {
+		//http://www.imooc.com/video/10082
 		 Connection con;
 	        //驱动程序名
 	        String driver = "com.mysql.jdbc.Driver";
